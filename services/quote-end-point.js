3fe7e32614da97eddf42d57d7f33f423ee0ed3fb
Nishant Desai <nishant.desai@mail.utoronto.ca>
2019-08-04 23:23:29 -0400

    finished quote end-point

--- services/quote-end-point.js
+++ services/quote-end-point.js
@@ -1,60 +1,153 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
 
+function getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, field) {
+
+    return axios.get(baseEndPoint, {
+            params: {
+                function: 'GLOBAL_QUOTE',
+                symbol: stockSymbol,
+                apikey: apiKey
+            }
+        })
+        .then(res => {
+
+            var price = res.data['Global Quote'][field];
+            return price;
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        });
+}
+
 module.exports = {
 
-    fetchPriceforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
+    fetchPriceforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
 
-        axios.get(base_endpoint, {
-                params: {
-                    function: 'GLOBAL_QUOTE',
-                    symbol: stockSymbol,
-                    apikey: apiKey
-                }
-            })
-            .then(res => {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '05. price')
+        .then(price => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
 
-                const params = {
-                    icon_emoji: ':stockbot:'
-                }
+            var response = 'The current price of security `' + stockSymbol + '` is `' + price + "USD`";
 
-                var price = res.data['Global Quote']['05. price'];
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
 
-                var response = 'The current price of security `' + stockSymbol + '` is `' + price + "USD`";
-
-                bot.postMessageToChannel(
-                    'general', 
-                    response,
-                    params
-                );
-            })
-            .catch(function(error) {
-                // handle error
-                console.log(error);
-            });
+        
     },
-    fetchOpenPriceforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
-        // Get open price for stock
 
-    },
-    fetchPreviousClosePriceforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
-        // Get previous close price for stock
+    fetchOpenPriceforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '02. open')
+        .then(price => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
 
-    },
-    fetchHighPriceforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
-        // Get open price for stock
+            var response = 'The current open price of security `' + stockSymbol + '` is `' + price + "USD`";
+
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
 
     },
-    fetchLowPriceforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
-        // Get open price for stock
+
+    fetchPreviousClosePriceforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '08. previous close')
+        .then(price => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
+
+            var response = 'The previous close price of security `' + stockSymbol + '` is `' + price + "USD`";
+
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
 
     },
-    fetchVolumeforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
-        // Get trading volume for stock
+
+    fetchHighPriceforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '03. high')
+        .then(price => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
+
+            var response = 'The previous high price of security `' + stockSymbol + '` is `' + price + "USD`";
+
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
 
     },
-    fetchChangeforStock: function(bot, base_endpoint, apiKey, stockSymbol) {
-        // Get change for stock
+
+    fetchLowPriceforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '04. low')
+        .then(price => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
+
+            var response = 'The previous low price of security `' + stockSymbol + '` is `' + price + "USD`";
+
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
+
+    },
+
+    fetchVolumeforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '06. volume')
+        .then(volume => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
+
+            var response = 'The current volume of security `' + stockSymbol + '` is `' + volume + "`";
+
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
+
+    },
+
+    fetchChangeforStock: function(bot, baseEndPoint, apiKey, stockSymbol) {
+        var promise = getFieldfromQuoteEndPoint(baseEndPoint, apiKey, stockSymbol, '10. change percent')
+        .then(change => {
+            const params = {
+                icon_emoji: ':ross:'
+            }
+
+            var response = 'The previous change percent of security `' + stockSymbol + '` is `' + change + "%`";
+
+            bot.postMessageToChannel(
+                'general', 
+                response,
+                params
+            );
+        });
 
     },
 
